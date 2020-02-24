d6697c0d1ae753379d3d1a9a068f771a059bf3c2
ankitSaebby <54979627+ankitSaebby@users.noreply.github.com>
2020-02-24 19:23:33 +0530

    Create q6.java

---
+++ q6.java
@@ -0,0 +1,21 @@
+//Q6) The sum of the squares of the first ten natural numbers is,
+//
+//12+22+...+102=385
+//The square of the sum of the first ten natural numbers is,
+//
+//(1+2+...+10)2=552=3025
+//Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025−385=2640.
+//
+//Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+public class q6 {
+	public static void main(String args[]) {int s1=0,s2=0;
+		for(int i=1;i<=100;i++) {
+			s1=s1+(i*i);
+			s2=s2+i;
+		}
+		s2=s2*s2;
+		int diff=s2-s1;
+		System.out.println("the difference between the two   "+diff);
+	}
+}
