557a8b141bdadb53de22ef6a8153756983f179b2
Ankit Singh <54979627+ankitSaebby@users.noreply.github.com>
2020-02-24 22:23:38 +0530

    Update q16.java

--- q16.java
+++ q16.java
@@ -3,18 +3,20 @@
 //     What is the sum of the digits of the number 2^1000?
 
 
+import java.math.BigInteger;
 public class q16 {
 	public static void main(String args[]) {
-	double x= Math.pow(2, 1000);
-	System.out.println(x);
-		double sum = 0, r;
-	    while( x!= 0 )
+		BigInteger f = new BigInteger("2");
+		BigInteger x=f.pow(1000);
+		BigInteger a= new BigInteger("10");
+        BigInteger s = new BigInteger("1");
+        BigInteger sum = new BigInteger("0");
+	    while( x.compareTo(s)>0 )
 	    {
-	        r=x % 10;
-	        sum=sum+r;
-	        x /= 10;
-	    }System.out.println(" the sum of the digits of the number 2^1000"+sum);
-
-	
+	    	s=x.remainder(a);
+            sum=sum.add(s);
+            x=x.divide(a);
+	    }System.out.println(sum);
 	}
 }
+
